3b9f38bc21f10fa119b6fb689c1603668a70ce65
Todor Dakov <tdd@dekom.bg>
2015-10-09 19:06:33 +0300

    New src - src3

---
+++ src3/Minesweeper/GameModel/Board/Field/Field.cs
@@ -0,0 +1,30 @@
+﻿namespace Minesweeper.GameModel.Board.Field
+{
+    using System;
+    using Utils;
+
+    public class Field
+    {
+        public const int MineContent = -1;
+
+        public Field(int content)
+        {
+            if (content < MineContent || Constants.MatrixColumn < content)
+            {
+                throw new ArgumentOutOfRangeException("Content is invalid - out fo range");
+            }
+
+            this.Content = content;
+        }
+
+        public int Content { get; private set; }
+
+        public bool IsMine
+        {
+            get
+            {
+                return Content == MineContent;
+            }
+        }
+    }
+}
---
+++ src3/Minesweeper/GameModel/Board/Field/FieldFactory.cs
@@ -0,0 +1,22 @@
+﻿namespace Minesweeper.GameModel.Board.Field
+{
+    using System.Collections.Generic;
+
+    public class FieldFactory
+    {
+        private Dictionary<int, Field> Fields = new Dictionary<int, Field>();
+
+        public Field GetField(int content)
+        {
+            Field field;
+
+            if (!this.Fields.TryGetValue(content, out field))
+            {
+                field = new Field(content);
+                this.Fields.Add(content, field);
+            }
+
+            return field;
+        }
+    }
+}
