60173e6b94dc2ad685b7363c166024a276454be0
tddold <tdd@dekom.bg>
2015-10-11 21:38:34 +0300

    Last commit

---
+++ src/Minesweeper/Minesweeper.Tests/ComsoleMinesweeperTests.cs
@@ -0,0 +1,21 @@
+﻿namespace Minesweeper.Tests
+{
+    using System;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using ConsoleMinesweeper;
+    using GameEngine;
+    [TestClass]
+    public class ComsoleMinesweeperTests
+    {
+        [TestMethod]
+        public void GetUIElementTest()
+        {
+            var fieldVisualisator = new FieldVisualator();
+            for (int i = 0; i < 8; i++)
+            {
+                var symbol = fieldVisualisator.GetUIElement(i);
+                Assert.AreEqual(i.ToString(), symbol);
+            }
+        }        
+    }
+}
